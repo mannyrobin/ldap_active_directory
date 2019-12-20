c86a87b4350bd0fdd0f320f3b4f8cc6fe9727ef6
mannyrobin <69haiqian@gmail.com>
2019-12-20 20:56:26 +1000

    First commit after complete

---
+++ app.js
@@ -0,0 +1,27 @@
+var ActiveDirectory = require('activedirectory');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+readline.question('User name: ', function(username) {
+    readline.question('Password: ', function(password) {
+        var config = {
+            url: 'ldap://10.0.0.4:389',
+            baseDN: 'cn=users,dc=desk,dc=radar',
+            username: username,
+            password: password
+        };
+        let ad = new ActiveDirectory(config), query='cn=*';
+        
+        ad.findUsers(query, true, function(err, users) {
+            if (err) {
+                console.log('ERROR: ' +JSON.stringify(err));
+            }
+            else if ((! users) || (users.length == 0)) console.log('No users found.');
+            else {
+                console.log(users);
+            }
+            readline.close();
+        });
+    })
+})
